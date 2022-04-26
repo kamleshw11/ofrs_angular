aa324d5024ecdaa5d0f00746be22909c15dcb275
Kamlesh <kamleshw@cybage.com>
2022-04-26 16:32:11 +0530

    flight component.ts and html modified for table

--- src/app/components/flight/flight.component.ts
+++ src/app/components/flight/flight.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,ViewChild} from '@angular/core';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
+import {MatTableDataSource} from '@angular/material/table';
 
 @Component({
   selector: 'app-flight',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./flight.component.scss']
 })
 export class FlightComponent implements OnInit {
+  displayedColumns: string[] = ['id', 'name', 'progress', 'fruit'];
+  dataSource!: MatTableDataSource<any>;
 
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
   constructor() { }
 
   ngOnInit(): void {
   }
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
 
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
 }
