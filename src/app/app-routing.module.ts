f82b9d000e5d8ad88905353f1b477cfac072dca3
Kamlesh <kamleshw@cybage.com>
2022-04-26 16:10:38 +0530

    navbar, flight, routing working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FlightComponent } from './components/flight/flight.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  // { path: 'user-list', component:  UserComponent},
+  { path: 'flight-list', component:  FlightComponent},
+  // { path: 'passenger-log', component:  PassengerComponent},
+  // { path: '', pathMatch: 'full', redirectTo: 'user-list' },
+  // { path: '**', component: PathNotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
