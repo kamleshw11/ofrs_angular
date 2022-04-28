0ad90efb81229b0160c6c25940b93820f017ea36
Jiva <jivaf@cybage.com>
2022-04-28 16:28:58 +0530

    User search flight component initiated

--- src/app/components/user-search-flight/user-search-flight.component.ts
+++ src/app/components/user-search-flight/user-search-flight.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup, FormControl,Form, Validators} from '@angular/forms';
+import {ErrorStateMatcher} from '@angular/material/core';
 
 @Component({
   selector: 'app-user-search-flight',
@@ -7,9 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserSearchFlightComponent implements OnInit {
 
-  constructor() { }
+  campaignOne: FormGroup;
+  campaignTwo: FormGroup;
+  constructor() {
+    const today = new Date();
+    const month = today.getMonth();
+    const year = today.getFullYear();
 
+    this.campaignOne = new FormGroup({
+      start: new FormControl(new Date(year, month, 13)),
+      end: new FormControl(new Date(year, month, 16)),
+    });
+
+    this.campaignTwo = new FormGroup({
+      start: new FormControl(new Date(year, month, 15)),
+      end: new FormControl(new Date(year, month, 19)),
+    });
+   }
+
+   selected = new FormControl('valid', [Validators.required, Validators.pattern('valid')]);
+
+   selectFormControl = new FormControl('valid', [Validators.required, Validators.pattern('valid')]);
+ 
+   nativeSelectFormControl = new FormControl('valid', [
+     Validators.required,
+     Validators.pattern('valid'),
+   ]);
+ 
+   matcher = new ErrorStateMatcher();
+   
   ngOnInit(): void {
   }
-
 }
