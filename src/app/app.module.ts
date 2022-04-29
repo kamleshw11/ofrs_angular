f87cfaa25cd71b29a588cf6b05fd95cbf7053bf2
Kamlesh <kamleshw@cybage.com>
2022-04-29 16:25:03 +0530

    Merge branch 'master' of github.com:kamleshw11/ofrs_angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,7 +15,21 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MaterialModule } from './material/material.module';
 import { AddFlightComponent } from './components/add-flight/add-flight.component';
 
+import { MAT_DATE_FORMATS } from '@angular/material/core';
+import { MatMomentDateModule } from '@angular/material-moment-adapter';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 
+const MY_FORMATS = {
+  parse: {
+    dateInput: 'YYYY-MM-DD',
+  },
+  display: {
+    dateInput: 'YYYY-MM-DD',
+    monthYearLabel: 'MM YYYY',
+    dateA11yLabel: 'YYYY-MM-DD',
+    monthYearA11yLabel: 'MM YYYY',
+  },
+};
 @NgModule({
   declarations: [
     AppComponent,
@@ -34,9 +48,14 @@ import { AddFlightComponent } from './components/add-flight/add-flight.component
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
-    MaterialModule
+    MaterialModule,
+    MatDatepickerModule,
+    MatMomentDateModule
   ],
-  providers: [],
+  
+  providers: [
+    { provide: MAT_DATE_FORMATS, useValue: MY_FORMATS }
+ ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
